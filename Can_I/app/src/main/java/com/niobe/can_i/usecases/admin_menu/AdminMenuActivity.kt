63ea3ed61697e55d6c6acfd9a661203ab4b8cf45
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-05-02 23:55:31 +0200

    CH-0004: Admin setOnClickListeners artículos

--- Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/AdminMenuActivity.kt
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/AdminMenuActivity.kt
@@ -1,13 +1,18 @@
 package com.niobe.can_i.usecases.admin_menu
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.niobe.can_i.R
+import com.niobe.can_i.usecases.admin_menu.admin_articulos.AdminGArticulosActivity
 
 class AdminMenuActivity : AppCompatActivity() {
+
+    private lateinit var botonGestionArticulos : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +22,24 @@ class AdminMenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Inicializamos los componentes
+        initUI()
+
+        // Configuramos el click listener para el botón
+        botonGestionArticulos.setOnClickListener {
+            // Aquí se ejecutará cuando se presione el botón
+
+        }
     }
+    //Función para inicializar todos los componentes del layout
+    private fun initUI(){
+        botonGestionArticulos = findViewById(R.id.bGestionArticulos)
+    }
+    // Función para navegar a AdminGArticulosActivity
+    private fun goToGestionArticulosActivity(){
+        // Iniciamos la actividad de destino
+        val intent = Intent(this, AdminGArticulosActivity::class.java)
+        startActivity(intent)
+    }
+
 }
