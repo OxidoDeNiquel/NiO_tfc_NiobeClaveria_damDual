86cb50e9d7498f7db753fb5e9709e9d1280c55ba
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-05-03 22:38:16 +0200

    CH-0004-v3: Admin setOnClickListeners artículos (con fixes)

--- Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/admin_articulos/GestionArticulosActivity.kt
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/admin_articulos/GestionArticulosActivity.kt
@@ -1,21 +1,50 @@
 package com.niobe.can_i.usecases.admin_menu.admin_articulos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.niobe.can_i.R
+import com.niobe.can_i.databinding.ActivityAdminMenuBinding
+import com.niobe.can_i.databinding.ActivityGestionArticulosBinding
+import com.niobe.can_i.usecases.admin_menu.admin_articulos.crear_articulo.CrearArticuloActivity
 
 class GestionArticulosActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityGestionArticulosBinding
+    private lateinit var botonCrear: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_gestion_articulos)
+        //Inicializamos el binding
+        binding = ActivityGestionArticulosBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Inicializamos los componentes
+        initUI()
+    }
+
+    //Función para inicializar todos los componentes del layout
+    private fun initUI(){
+        botonCrear = binding.bAnadirArticulo
+        // Configuramos el click listener para el botón
+        botonCrear.setOnClickListener {
+            goToCrearArticulosActivity()
+        }
+    }
+
+    // Función para navegar a AdminGArticulosActivity
+    private fun goToCrearArticulosActivity(){
+        // Iniciamos la actividad de destino
+        val intent = Intent(this, CrearArticuloActivity::class.java)
+        startActivity(intent)
     }
 }
--- Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/admin_articulos/GestionArticulosViewHolder.kt
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/admin_articulos/GestionArticulosViewHolder.kt
@@ -3,13 +3,13 @@ package com.niobe.can_i.usecases.admin_menu.admin_articulos
 import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.niobe.can_i.database.entity.ArticulosEntity
 import com.niobe.can_i.databinding.ItemProductsBinding
+import com.niobe.can_i.model.Articulo
 
 class GestionArticulosViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private var binding = ItemProductsBinding.bind(view)
 
-    fun bind(articulosEntityResponse: ArticulosEntity){
+    fun bind(articulosEntityResponse: Articulo){
         binding.tvArticulo.text = articulosEntityResponse.nombre
     }
 }
