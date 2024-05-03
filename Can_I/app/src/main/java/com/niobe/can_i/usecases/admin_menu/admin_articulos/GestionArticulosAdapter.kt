ce42f450e10b7606324a5aeb84a692949ac27563
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-05-03 22:04:03 +0200

    CH-0004: Admin setOnClickListeners artículos (con fixes)

---
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/admin_menu/admin_articulos/GestionArticulosAdapter.kt
@@ -0,0 +1,29 @@
+package com.niobe.can_i.usecases.admin_menu.admin_articulos
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.niobe.can_i.R
+import com.niobe.can_i.database.entity.ArticulosEntity
+
+class GestionArticulosAdapter(var moviesList: List<ArticulosEntity> = emptyList()): RecyclerView.Adapter<GestionArticulosViewHolder>() {
+    fun updateList(list: List<ArticulosEntity>){
+        moviesList = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GestionArticulosViewHolder {
+        return GestionArticulosViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_products, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: GestionArticulosViewHolder, position: Int) {
+        holder.bind(moviesList[position])
+    }
+
+    override fun getItemCount(): Int = moviesList.size
+
+}
