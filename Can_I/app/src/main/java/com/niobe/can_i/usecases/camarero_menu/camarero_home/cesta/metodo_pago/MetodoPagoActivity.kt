76a332892afbe101c89ce5d725cf33370d15a3cf
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-05-18 23:42:41 +0200

    CH-0025: Nuevas activities

---
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/camarero_menu/camarero_home/cesta/metodo_pago/MetodoPagoActivity.kt
@@ -0,0 +1,21 @@
+package com.niobe.can_i.usecases.camarero_menu.camarero_home.cesta.metodo_pago
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.niobe.can_i.R
+
+class MetodoPagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_metodo_pago)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/camarero_menu/camarero_home/cesta/metodo_pago/resultado_operacion/OK_Activity.kt
@@ -0,0 +1,21 @@
+package com.niobe.can_i.usecases.camarero_menu.camarero_home.cesta.metodo_pago.resultado_operacion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.niobe.can_i.R
+
+class OK_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ok)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
