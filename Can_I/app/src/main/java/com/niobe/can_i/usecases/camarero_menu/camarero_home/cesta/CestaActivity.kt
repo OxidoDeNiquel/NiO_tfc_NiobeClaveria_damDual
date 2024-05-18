4f6f2e2cd2f1cfac1d74d5fb58f24c2601730bb4
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-05-18 20:06:28 +0200

    CH-0022: Implementación de precio total en la cesta

--- Can_I/app/src/main/java/com/niobe/can_i/usecases/camarero_menu/camarero_home/cesta/CestaActivity.kt
+++ Can_I/app/src/main/java/com/niobe/can_i/usecases/camarero_menu/camarero_home/cesta/CestaActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.niobe.can_i.R
 import com.niobe.can_i.databinding.ActivityCestaBinding
+import com.niobe.can_i.model.ArticulosComanda
 import com.niobe.can_i.provider.services.firebase.FirebaseUtil
 import com.niobe.can_i.usecases.camarero_menu.camarero_home.sel_articulo.SelArticuloCamareroActivity
 import com.niobe.can_i.util.Constants
@@ -62,10 +63,41 @@ class CestaActivity : AppCompatActivity() {
 
     private fun loadArticulosComanda(idComanda: String) {
         firebaseUtil.getArticulosComandaByComanda(idComanda) { articulosComandaList ->
-            Log.i("ArticulosComanda", "Size: ${articulosComandaList.size}")
             adapter.updateList(articulosComandaList)
+            showTotalPrice(articulosComandaList) // Llama a la función showTotalPrice después de actualizar la lista de artículos
         }
     }
+
+
+    private fun showTotalPrice(articulosComandaList: List<ArticulosComanda>) {
+        var totalPrice = 0.0
+        var loadedArticulos = 0 // Para llevar el conteo de cuántos artículos se han cargado
+
+        // Recorre la lista de artículos de la cesta
+        for (articuloComanda in articulosComandaList) {
+            // Obtiene el artículo correspondiente al ID del artículo de la cesta
+            firebaseUtil.getArticuloById(articuloComanda.idArticulo.articuloId) { articulo ->
+                // Verifica si se encontró el artículo y si tiene un precio válido
+                if (articulo != null && articulo.precio > 0) {
+                    // Incrementa el total sumando el precio del artículo multiplicado por la cantidad
+                    totalPrice += articulo.precio * articuloComanda.cantidad
+                }
+
+                // Incrementa el contador de artículos cargados
+                loadedArticulos++
+
+                // Comprueba si ya se han cargado todos los artículos
+                if (loadedArticulos == articulosComandaList.size) {
+                    // Actualiza el TextView con el precio total
+                    binding.tvTotalPrecio.text = buildString {
+                        append(totalPrice.toString())
+                        append("€")
+                    }
+                }
+            }
+        }
+    }
+
 
     private fun navigateToDetail(id: String) {
         val intent = Intent(this, SelArticuloCamareroActivity::class.java)
